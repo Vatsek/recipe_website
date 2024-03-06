65f7c47a1b0e29800a5775caa643dea25b5dd525
Vatsek Pavel <pvatsek@gmail.com>
2024-03-06 13:15:09 +0300

    reg complete

--- usersapp/forms.py
+++ usersapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
 class LoginUserForm(forms.Form):
@@ -6,3 +7,36 @@ class LoginUserForm(forms.Form):
                                widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Пароль',
                                widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+
+class RegisterForm(forms.ModelForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        labels = {
+            'email': 'E-mail',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия'
+        }
+
+    widgets = {
+        'email': forms.TextInput(attrs={'class': 'form-input'}),
+        'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+        'last_name': forms.TextInput(attrs={'class': 'form-input'}),
+    }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cd['password']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError('Пользователь с таким E-mail уже существует')
+        return email
